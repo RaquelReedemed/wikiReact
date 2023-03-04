29feb67ccc43de90077f7e30f0906978d588172c
Raquel Reedemed <raquel.reedemed@gmail.com>
2023-03-04 16:58:26 -0300

    toggler

--- src/components/navbareq.jsx
+++ src/components/navbareq.jsx
@@ -1,19 +1,30 @@
 import '../css/style.css'
 
-import React from 'react'
+import React, { useState } from 'react'
 
 
 function Navbareq() {
+
+  const [active, setActive] = useState('navUl');
+
+  const navHamb = () => {
+    active === 'navUl'
+    ? setActive('navUl navActive')
+    : setActive('navUl')
+  }
+
+   
+
   return (
     <nav className='nav'>
-    <div className='navLogo'><a  href='#' ><img src='https://res.cloudinary.com/da5fzpyjp/image/upload/v1677629421/wiki/logo1_lchefc.jpg' alt='logo'></img></a></div>
-     <ul className='navUl' >
-       <div><li className='navLi'><a href='#'>Arte y Filosofia</a></li></div>
-       <div><li className='navLi'><a href='#'>Historia y cultura</a></li></div>
-       <div><li className='navLi'><a href='#'>Geografia y naturaleza</a></li></div>
-       <div><li className='navLi'><a href='#'>Disenio UX y UI</a></li></div>
+    <div className='navLogo'><a  href='#'><img src='https://res.cloudinary.com/da5fzpyjp/image/upload/v1677629421/wiki/logo1_lchefc.jpg' alt='logo'></img></a></div>
+     <ul className={active}>
+       <li className='navLi'><a href='#'>Arte y Filosofia</a></li>
+       <li className='navLi'><a href='#'>Historia y cultura</a></li>
+       <li className='navLi'><a href='#'>Geografia y naturaleza</a></li>
+       <li className='navLi'><a href='#'>Disenio UX y UI</a></li>
      </ul>
-     <div className='navToggler' >
+     <div onClick={navHamb} className='navToggler'>
      <i class="bi bi-grid-3x3-gap-fill"></i>
      </div>
     </nav>
